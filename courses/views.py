4f20b6787076d34eb0405b4bfa40b4385d2bc2d6
Dominik <droko@gmx.de>
2022-05-09 07:10:20 +0200

    Added log-in and log-out functionality

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+import django.views
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Course
 
 
+class OwnerMixin:
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(owner=self.request.user)
+
+
+class OwnerEditMixin:
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+class OwnerCourseMixin:
+    model = Course
+    fields = ["subject", "title", "slug", "overview"]
+    success_url = reverse_lazy("manage_course_list")
+
+
+class ManageCourseListView(ListView):
+    model = Course
+    template_name = "courses/manage/course/list.html"
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(owner=self.request.user)
